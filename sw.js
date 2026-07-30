b7dacec2d9827ef6dc0d8eef594c7306b8653afb
Boris Romão Antunes <62909074+BorisRomaoAntunes@users.noreply.github.com>
2026-07-30 10:25:43 -0300

    fix: remove sombra avermelhada do ícone dos botões temporários

--- sw.js
+++ sw.js
@@ -91,7 +91,7 @@ self.addEventListener('notificationclick', function(event) {
 // ==========================================
 // CONFIGURAÇÃO DO PWA & CACHE OFFLINE
 // ==========================================
-const CACHE_NAME = 'oer-agenda-v3';
+const CACHE_NAME = 'oer-agenda-v4';
 const ASSETS_TO_CACHE = [
     '/',
     '/index.html',
